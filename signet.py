6062faf63f9d566bbe1cde5ebf25d952aa349ba8
Thanh Pham Ngoc <19520958@gm.uit.edu.vn>
2022-11-25 22:42:09 +0700

    UPDATE LICENSE

--- signet.py
+++ signet.py
@@ -1,13 +1,7 @@
-
-
-#  MIT License
 #  Copyright (c) 2022. Thanh Pham Ngoc <phmngcthanh <AT>gmail.com>
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#  All rights reserved.
+#  A copy of the license can be found at the LICENSE file, or the link https://github.com/phmngcthanh/jetson-net/blob/master/LICENSE
+
 
 # Jetson Nano SmartGate for Wens course@UIT - guided by Assoc.Prof. Le Trung Quan#
 # This is Jetson-Python part. The full solutions includes Jetson-Cython, ESP8266 #
